861f07b99a376c0d3286d26a09db4027e28ff594
Vitalii Parovishnyk <korich.vi.p@gmail.com>
2020-12-03 20:59:05 +0200

    xcode project init

---
+++ Source/Lock_WatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lock_WatcherApp.swift
+//  Lock-Watcher
+//
+//  Created by Vitalii Parovishnyk on 03.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct Lock_WatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
